eba9ee2c25b32924aa7e13818ed99d457fa6a9b7
calinvladth <calinvladth@icloud.com>
2022-11-02 07:43:46 +0200

    Section 6 (Drawer implementation)

--- App.js
+++ App.js
@@ -6,9 +6,11 @@ import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import MealDetailsScreen from "./screens/MealDetailsScreen";
 import {Text} from "react-native";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import FavoriteScreen from "./screens/FavoriteScreen";
 
 const Stack = createNativeStackNavigator()
-// const Drawer = createDrawerNavigator()
+
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -20,20 +22,22 @@ export default function App() {
         return <Text>Loading...</Text>
     }
 
-    // function drawerNavigator() {
-    //     return <Drawer.Navigator>
-    //         <Drawer.Screen name="Categories" component={CategoriesScreen}/>
-    //         <Drawer.Screen name="Categories" component={FavoriteScreen}/>
-    //     </Drawer.Navigator>
-    // }
+    const Drawer = createDrawerNavigator()
+
+    function DrawerNavigator() {
+        return <Drawer.Navigator>
+            <Drawer.Screen name="Categories" component={CategoriesScreen}/>
+            <Drawer.Screen name="Favorites" component={FavoriteScreen}/>
+        </Drawer.Navigator>
+    }
 
     return <>
         <StatusBar style={"dark"}/>
         <NavigationContainer>
             <Stack.Navigator initialRouteName={"MealsCategories"}>
                 <Stack.Screen name={"MealsCategories"}
-                              component={CategoriesScreen}
-                              options={{title: 'All Categories'}}/>
+                              component={DrawerNavigator}
+                              options={{headerShown: false}}/>
                 <Stack.Screen name={"MealsOverview"}
                               component={MealsOverviewScreen}
                               options={{title: 'Overview', headerBackTitle: "Back"}}
