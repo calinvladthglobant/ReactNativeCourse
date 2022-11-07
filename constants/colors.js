ca0507057710413e3ae1d21e015a8da50c5e6078
calinvladth <calinvladth@icloud.com>
2022-11-07 07:49:17 +0200

    Clean main

--- constants/colors.js
+++ constants/colors.js
@@ -1,10 +1,6 @@
 const Colors = {
-    primary500: '#72063c',
-    primary600: '#640233',
-    primary700: '#4e0329',
-    primary800: '#4e0329',
-    accent500: '#ddb52f',
-    white: '#FFFFFF'
+    white: '#FFFFFF',
+    black: '#000000'
 }
 
 export default Colors
